d8e76ca918a0f8ee49891113a12b5ca10463c3ba
cetlewa <cetlewa@gmail.com>
2022-02-26 21:27:16 +0800

    initial

---
+++ cmake-build-debug/CMakeFiles/hw1_grammar_analysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw1_grammar_analysis.dir/testfile1.c.o"
+  "hw1_grammar_analysis"
+  "hw1_grammar_analysis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw1_grammar_analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
